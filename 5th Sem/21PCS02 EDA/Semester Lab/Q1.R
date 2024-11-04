941703b35538611836d6b2b4d2054262edbfb66e
Vishwanath61 <120776288+Vishwanath61@users.noreply.github.com>
2024-11-05 02:54:53 +0530

    Update Q1.R

--- 5th Sem/21PCS02 EDA/Semester Lab/Q1.R
+++ 5th Sem/21PCS02 EDA/Semester Lab/Q1.R
@@ -2,8 +2,9 @@
 
 co2_data<-read.csv("CO2.csv")
 str(co2_data)
+head(co2_data)
 
-hypotenuse_mean<-30
+hypotenuse_mean<-20
 one_sample_test<-t.test(co2_data$uptake,mu=hypotenuse_mean)
 print(one_sample_test)
 
@@ -11,3 +12,9 @@ uptake_chilled<-co2_data$uptake[co2_data$Treatment=="chilled"]
 uptake_nonchilled<-co2_data$uptake[co2_data$Treatment=="nonchilled"]
 two_sample_test<-t.test(uptake_chilled,uptake_nonchilled,var.equal=TRUE)
 print(two_sample_test)
+
+# Boxplot to compare uptake between treatments
+ggplot(co2_data, aes(x = Treatment, y = uptake, fill = Treatment)) +
+  geom_boxplot() +
+  labs(title = "CO2 Uptake by Treatment Type", x = "Treatment", y = "Uptake") +
+  theme_minimal()
