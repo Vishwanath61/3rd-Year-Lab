c1841e9655fb62829dcef74fca92ac43d62c13d7
Vishwanath61 <120776288+Vishwanath61@users.noreply.github.com>
2024-11-04 19:53:09 +0530

    Create Q4.R

---
+++ 5th Sem/21PCS02 EDA/Semester Lab/Q4.R
@@ -0,0 +1,25 @@
+#4.Decision Tree
+
+iris_data<-read.csv("Iris.csv")
+head(iris_data)
+
+sample_data<-sample(1:nrow(iris_data),0.7*nrow(iris_data))
+
+train_data<-iris_data[sample_data,]
+test_data<-iris_data[-sample_data,]
+
+library(rpart)
+
+model<-rpart(data = train_data,Species~Sepal.Length+Sepal.Width+Petal.Length+Petal.Width,
+             method = "class")
+
+library(rpart.plot)
+
+rpart.plot(model,main="Decision tree")
+
+prediction<-predict(model,test_data,type="class")
+confusion_matrix<-table(test_data$Species,prediction)
+print(confusion_matrix)
+
+accuracy<-sum(diag(confusion_matrix))/sum(confusion_matrix)
+print(accuracy*100)
