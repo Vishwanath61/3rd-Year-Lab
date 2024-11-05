5053a96e0ab696c4bee399ef7a70dee933ca1ca5
Vishwanath61 <120776288+Vishwanath61@users.noreply.github.com>
2024-11-06 04:36:36 +0530

    Create Q4_BayesianNetwork.py

---
+++ 5th Sem/21CS303 AIML/Semester Problems/Q4_BayesianNetwork.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+from pgmpy.models import BayesianNetwork
+from pgmpy.inference import VariableElimination
+from pgmpy.estimators import MaximumLikelihoodEstimator
+pd.read_csv("HeartDisease.csv")
+model=BayesianNetwork([
+    ('age','heartdisease'),
+    ('cp','heartdisease'),
+    ('chol','heartdisease'),
+    ('gender','heartdisease'),
+    ('restecg','heartdisease'),
+    ('exang','heartdisease'),
+])
+model.fit(data,estimator=MaximumLikelihoodEstimator)
+inference=VariableElimination(model)
+print(inference.query(variables=['heartdisease'], evidence={'cp':2}))
